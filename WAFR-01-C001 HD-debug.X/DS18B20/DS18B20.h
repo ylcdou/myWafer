08c3f96106fdf02c89fdc26472f6e54dde10d533
LawrenceYao <ylcdou@gmail.com>
2015-10-26 10:08:45 -0400

    2015-10-26 commit , Actually no change

--- WAFR-01-C001 HD-debug.X/DS18B20/DS18B20.h
+++ WAFR-01-C001 HD-debug.X/DS18B20/DS18B20.h
@@ -16,32 +16,12 @@
 #define DS18B20_EXT extern
 #endif
 
-/******************************************************************************/
-/* User Level #define Macros                                                  */
-/******************************************************************************/
-
-#define TRUE    1
-#define FALSE   0  
-
-#define YES     1
-#define NO      0     
-
-#define INPUT   1
-#define OUTPUT  0
-
-#define ON      1    // in this system , Led = 1 ---> turn on
-#define OFF     0
 
 /******************************************************************************/
 /* User Function Prototypes                                                   */
 /******************************************************************************/
 
-/* TODO User level functions prototypes (i.e. InitApp) go here */
-void delay_s(unsigned int x) ;
-void delay_ms(unsigned int x) ;
-void delay_10us (void) ;
-
-#endif   // _USER_H_
+#endif   // _DS18B20_H_
 
 
 //*****************************************************************************
